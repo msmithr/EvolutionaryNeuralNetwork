fb2707b269f60f44c6fca004202c032658963c7a
threough <michael.r.smith@siu.edu>
2017-11-01 20:26:11 -0500

    Documenting NN

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,10 @@
-
+import NeuralNetwork.NeuralNetwork;
 
 public class Driver {
 	public static void main(String[] args) {
 		NeuralNetwork n = new NeuralNetwork(new double[] {1,2,3,4,5,6,7,8,9,10,11,12}, 3, 1, 2, 2);
 		System.out.println(n);
 
-		n.feedForward(new double[] {0,0});
-		System.out.println(n.getResults()[0]);
-		n.feedForward(new double[] {1,0});
-		System.out.println(n.getResults()[0]);
-		n.feedForward(new double[] {0,1});
-		System.out.println(n.getResults()[0]);
-		n.feedForward(new double[] {1,1});
-		System.out.println(n.getResults()[0]);
+		System.out.println(n.feedForward(new double[] {1,1})[0]);
 	}
 }
--- src/NeuralNetwork/Neuron.java
+++ src/NeuralNetwork/Neuron.java
@@ -1,33 +1,55 @@
-
+package NeuralNetwork;
 
 public class Neuron {
 	private double output;
 	private double[] weights;
 	private int index;
 	
+	/**
+	 * Neuron class for the neural network
+	 * @param index The location of the neuron within it's layer
+	 * @param weights The array of doubles representing this neurons weights
+	 */
 	public Neuron(int index, double[] weights) {
 		this.index = index;
 		this.weights = weights;
 	}
 	
+	/**
+	 * Setter function for output
+	 * @param output Value for output
+	 */
 	public void setOutput(double output) {
 		this.output = output;
 	}
 	
+	/**
+	 * Getter function for output
+	 * @return The current output value
+	 */
 	public double getOutput() {
 		return output;
 	}
 	
+	/**
+	 * Getter function for a specific weight
+	 * @param index The index of the weight to get
+	 * @return The weight of that connection
+	 */
 	public double getWeight(int index) {
 		return weights[index];
 	}
 	
+	/**
+	 * Feed forward given the previous layer's outputs
+	 * @param prevLayer An array of neurons representing the previous, already activated, layer
+	 */
 	public void feedForward(Neuron[] prevLayer) {
 		double sum = 0;
 		for (int i = 0; i < prevLayer.length; i++) {
 			sum += prevLayer[i].getOutput() * prevLayer[i].getWeight(index);
 		}
-		output = MathLib.tanh(sum);
+		output = Math.tanh(sum);
 	}
 	
 	public String toString() {
