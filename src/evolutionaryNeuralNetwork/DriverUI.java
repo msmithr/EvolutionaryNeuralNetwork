eae80db711405ebc4928d335ee2042f31fc61e54
Nicholaslasswell <nicholas.lasswell@siu.edu>
2017-12-03 20:50:58 -0600

    Functionality added split training
    Added functionality to use either .optimize() or .optimizeUntil()

--- src/evolutionaryNeuralNetwork/DriverUI.java
+++ src/evolutionaryNeuralNetwork/DriverUI.java
@@ -34,7 +34,7 @@ public class DriverUI extends JFrame {
 	private JTextField textFieldInput;
 	private JButton btnStop;
 	private JComboBox<String> comboBoxAF;
-	private JButton btnTrain;
+	private JButton btnTrainIterations;
 	private JTextField textFieldFileName;
 	private JButton btnFind;
 	private JButton btnLoad;
@@ -42,6 +42,8 @@ public class DriverUI extends JFrame {
 	private DataSet learningData;
 	public NeuralNetwork result;
 	public GeneticAlgorithm moon;
+	private JTextField textFieldNIterations;
+	private JTextField textFieldErrorUntil;
 
 	/**
 	 * Launch the application.
@@ -90,20 +92,28 @@ public class DriverUI extends JFrame {
 		comboBoxAF.setBounds(187, 303, 86, 20);
 		contentPane.add(comboBoxAF);
 		
-		btnTrain = new JButton("Train");
-		btnTrain.addActionListener(new ActionListener() {
+		btnTrainIterations = new JButton("Train For Iterations");
+		btnTrainIterations.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("running");
-				new TrainWorker().execute();
-				System.out.println("ran");
+				new TrainIterationsWorker().execute();
 			}
 		});
-		btnTrain.setBounds(347, 302, 89, 23);
-		btnTrain.setEnabled(false);
-		contentPane.add(btnTrain);
+		btnTrainIterations.setBounds(283, 327, 153, 23);
+		btnTrainIterations.setEnabled(false);
+		contentPane.add(btnTrainIterations);
+		
+		JButton btnTrainUntilError = new JButton("Train Until Error");
+		btnTrainUntilError.setEnabled(false);
+		btnTrainUntilError.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				new TrainUntilWorker().execute();
+			}
+		});
+		btnTrainUntilError.setBounds(283, 352, 153, 23);
+		contentPane.add(btnTrainUntilError);
 		
 		btnStop = new JButton("Stop");
-		btnStop.setBounds(347, 268, 89, 23);
+		btnStop.setBounds(283, 300, 153, 23);
 		btnStop.setEnabled(false);
 		btnStop.addActionListener(new ActionListener() {
 			@Override
@@ -160,7 +170,8 @@ public class DriverUI extends JFrame {
 				}
 				
 				learningData = dataset;
-				btnTrain.setEnabled(true);
+				btnTrainIterations.setEnabled(true);
+				btnTrainUntilError.setEnabled(true);
 			}
 		});
 		contentPane.add(btnLoad);
@@ -295,11 +306,11 @@ public class DriverUI extends JFrame {
 		contentPane.add(btnQuery);
 		
 		JButton btnSaveNN = new JButton("Save NN");
-		btnSaveNN.setBounds(10, 400, 89, 23);
+		btnSaveNN.setBounds(10, 400, 113, 23);
 		contentPane.add(btnSaveNN);
 		
 		JButton btnLoadNn = new JButton("Load NN");
-		btnLoadNn.setBounds(10, 425, 89, 23);
+		btnLoadNn.setBounds(10, 425, 113, 23);
 		contentPane.add(btnLoadNn);
 		
 		JLabel lblError = new JLabel("Error");
@@ -310,6 +321,27 @@ public class DriverUI extends JFrame {
 		textPaneError.setEditable(false);
 		textPaneError.setBounds(487, 109, 199, 286);
 		contentPane.add(textPaneError);
+		
+		JLabel lblNumberOfIterations = new JLabel("Number of Iterations");
+		lblNumberOfIterations.setBounds(10, 331, 127, 14);
+		contentPane.add(lblNumberOfIterations);
+		
+		textFieldNIterations = new JTextField();
+		textFieldNIterations.setText("5000");
+		textFieldNIterations.setBounds(187, 328, 86, 20);
+		contentPane.add(textFieldNIterations);
+		textFieldNIterations.setColumns(10);
+		
+		JLabel lblErrorToOptimize = new JLabel("Error to Optimize Until");
+		lblErrorToOptimize.setBounds(10, 356, 127, 14);
+		contentPane.add(lblErrorToOptimize);
+		
+		textFieldErrorUntil = new JTextField();
+		textFieldErrorUntil.setText("25.50");
+		textFieldErrorUntil.setBounds(187, 353, 86, 20);
+		contentPane.add(textFieldErrorUntil);
+		textFieldErrorUntil.setColumns(10);
+	
 	}
 	
 	private boolean validateInputs() {
@@ -355,7 +387,7 @@ public class DriverUI extends JFrame {
 		return true;	
 	}
 	
-	class TrainWorker extends SwingWorker<Void, Void> {
+	class TrainIterationsWorker extends SwingWorker<Void, Void> {
 		@Override
 		protected Void doInBackground() throws Exception {
 			btnStop.setEnabled(true);
@@ -368,6 +400,8 @@ public class DriverUI extends JFrame {
 			textFieldMutation.setEditable(false);
 			textFieldTournamentSize.setEditable(false);
 			comboBoxAF.setEditable(false);
+			textFieldNIterations.setEditable(false);
+			textFieldErrorUntil.setEditable(false);
 			
 			if (!validateInputs()) {
 				textFieldNInputs.setEditable(true);
@@ -379,6 +413,8 @@ public class DriverUI extends JFrame {
 				textFieldMutation.setEditable(true);
 				textFieldTournamentSize.setEditable(true);
 				comboBoxAF.setEditable(true);
+				textFieldNIterations.setEditable(true);
+				textFieldErrorUntil.setEditable(true);
 				btnStop.setEnabled(false);
 				JOptionPane.showMessageDialog(null, "Invalid Inputs");
 				return null;
@@ -420,11 +456,14 @@ public class DriverUI extends JFrame {
 				af = ActivationFunction.SIGMOID_STEP;
 			}
 			
+			String iterateTemp = textFieldNIterations.getText();
+			int iterations = Integer.parseInt(iterateTemp);
+			
 			GeneticAlgorithm moon = new GeneticAlgorithm(nInputs, nOutputs, nLayers, nNeurons, 
 					popSize, crossoverPropability, mutationPropability, 
 					tournSize, learningData, af);
 			
-			result = moon.optimize(50000);
+			result = moon.optimize(iterations);
 			
 			textFieldNInputs.setEditable(true);
 			textFieldNOutputs.setEditable(true);
@@ -435,6 +474,104 @@ public class DriverUI extends JFrame {
 			textFieldMutation.setEditable(true);
 			textFieldTournamentSize.setEditable(true);
 			comboBoxAF.setEditable(true);
+			textFieldNIterations.setEditable(true);
+			textFieldErrorUntil.setEditable(true);
+			btnStop.setEnabled(false);
+			return null;
+			
+		}
+		
+	}
+	
+	class TrainUntilWorker extends SwingWorker<Void, Void> {
+		@Override
+		protected Void doInBackground() throws Exception {
+			btnStop.setEnabled(true);
+			textFieldNInputs.setEditable(false);
+			textFieldNOutputs.setEditable(false);
+			textFieldLayers.setEditable(false);
+			textFieldNeuronsLayer.setEditable(false);
+			textFieldPopSize.setEditable(false);
+			textFieldCrossover.setEditable(false);
+			textFieldMutation.setEditable(false);
+			textFieldTournamentSize.setEditable(false);
+			comboBoxAF.setEditable(false);
+			textFieldNIterations.setEditable(false);
+			textFieldErrorUntil.setEditable(false);
+			
+			if (!validateInputs()) {
+				textFieldNInputs.setEditable(true);
+				textFieldNOutputs.setEditable(true);
+				textFieldLayers.setEditable(true);
+				textFieldNeuronsLayer.setEditable(true);
+				textFieldPopSize.setEditable(true);
+				textFieldCrossover.setEditable(true);
+				textFieldMutation.setEditable(true);
+				textFieldTournamentSize.setEditable(true);
+				comboBoxAF.setEditable(true);
+				textFieldNIterations.setEditable(true);
+				textFieldErrorUntil.setEditable(true);
+				btnStop.setEnabled(false);
+				JOptionPane.showMessageDialog(null, "Invalid Inputs");
+				return null;
+			}
+			
+			String inputTemp = textFieldNInputs.getText();
+			int nInputs = Integer.parseInt(inputTemp);
+			
+			String outputTemp = textFieldNOutputs.getText();
+			int nOutputs = Integer.parseInt(outputTemp);
+			
+			String layerTemp = textFieldLayers.getText();
+			int nLayers = Integer.parseInt(layerTemp);
+			
+			String nLayerTemp = textFieldNeuronsLayer.getText();
+			int nNeurons = Integer.parseInt(nLayerTemp);
+			
+			String popTemp = textFieldPopSize.getText();
+			int popSize = Integer.parseInt(popTemp);
+			
+			String crossTemp = textFieldCrossover.getText();
+			double crossoverPropability = Double.parseDouble(crossTemp);
+			
+			String mutantTemp = textFieldMutation.getText();
+			double mutationPropability = Double.parseDouble(mutantTemp);
+			
+			String tournamentTemp = textFieldTournamentSize.getText();
+			int tournSize = Integer.parseInt(tournamentTemp);
+			
+			String afTemp = (String) comboBoxAF.getSelectedItem();
+			ActivationFunction af;
+			if (afTemp == "sigmoid"){
+				af = ActivationFunction.SIGMOID;
+			} else if (afTemp == "step") {
+				af = ActivationFunction.STEP;
+			} else if (afTemp == "tanh") {
+				af = ActivationFunction.TANH;
+			} else {
+				af = ActivationFunction.SIGMOID_STEP;
+			}
+			
+			String errorTemp = textFieldErrorUntil.getText();
+			double error = Double.parseDouble(errorTemp);
+			
+			GeneticAlgorithm moon = new GeneticAlgorithm(nInputs, nOutputs, nLayers, nNeurons, 
+					popSize, crossoverPropability, mutationPropability, 
+					tournSize, learningData, af);
+			
+			result = moon.optimizeUntil(error);
+			
+			textFieldNInputs.setEditable(true);
+			textFieldNOutputs.setEditable(true);
+			textFieldLayers.setEditable(true);
+			textFieldNeuronsLayer.setEditable(true);
+			textFieldPopSize.setEditable(true);
+			textFieldCrossover.setEditable(true);
+			textFieldMutation.setEditable(true);
+			textFieldTournamentSize.setEditable(true);
+			comboBoxAF.setEditable(true);
+			textFieldNIterations.setEditable(true);
+			textFieldErrorUntil.setEditable(true);
 			btnStop.setEnabled(false);
 			return null;
 			
